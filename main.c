81f7dec290fb7cd6f838758ec1caf9f16a3f9274
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2020-01-14 16:42:59 -0300

    Define Piece enum

--- main.c
+++ main.c
@@ -2,5 +2,14 @@
 
 int main(int argc, char *argv[])
 {
+	// This is the enum which will fill the board array
+	// maybe in the future more options will be added 
+	// for other purposes (highlighting for analysis,
+	// etc.)
+	enum Piece {
+		EMPTY_PIECE,
+		RED_PIECE,	// Red plays first
+		BLUE_PIECE,	// Blue plays second
+	};
 	return 0;
 }
