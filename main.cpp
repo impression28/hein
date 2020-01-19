6f06a1e08b1fc6591a5156b8cc0c4046e8768bfb
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2020-01-19 18:59:18 -0300

    Test empty board printing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <assert.h>
 
+#define DEBUG
 // This is the enum which will fill the board array
 // maybe in the future more options will be added 
 // for other purposes (highlighting for analysis,
@@ -17,11 +19,38 @@ std::string DrawBoard(std::vector <std::vector <Piece>> board);
 
 int main(int argc, char *argv[])
 {
-
+	std::vector <Piece> v(11, EMPTY_PIECE);
+	std::vector <std::vector <Piece>> board(11, v);
+	std::cout << DrawBoard(board);
 	return 0;
 }
 
+// Preconditions: board must be 11x11
+
 std::string DrawBoard(std::vector <std::vector <Piece>> board) 
 {
-	return std::string {""};
+	// Test preconditions
+	#ifdef DEBUG
+	assert(board.size() == 11);
+	for (size_t i = 0; i < 11; i++)
+		assert(board.at(i).size() == 11);
+	#endif
+
+	// #TODO: suppport boards of different sizes
+	std::string board_template = std::string {\
+	" A B C D E F G H I J K L\n"
+	"  • • • • • • • • • • • • •\n"
+	" 1 • · · · · · · · · · · · •\n"
+	"  2 • · · · · · · · · · · · •\n"
+	"   3 • · · · · · · · · · · · •\n"
+	"    4 • · · · · · · · · · · · •\n"
+	"     5 • · · · · · · · · · · · •\n"
+	"      6 • · · · · · · · · · · · •\n"
+	"       7 • · · · · · · · · · · · •\n"
+	"        8 • · · · · · · · · · · · •\n"
+	"         9 • · · · · · · · · · · · •\n"
+	"         10 • · · · · · · · · · · · •\n"
+	"          11 • · · · · · · · · · · · •\n"
+	"              • • • • • • • • • • • • •\n"};
+	return std::string {board_template};
 }
