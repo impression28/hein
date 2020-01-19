014da63dd76d92e310860fe9c0914681f7af10f0
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2020-01-19 18:32:43 -0300

    Declare function to draw the board

--- main.cpp
+++ main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+// This is the enum which will fill the board array
+// maybe in the future more options will be added 
+// for other purposes (highlighting for analysis,
+// etc.)
+typedef enum 
+{
+	EMPTY_PIECE,	// Default state with no piece
+	RED_PIECE,	// Red plays first
+	BLUE_PIECE,	// Blue plays second
+} Piece;
+
+std::string DrawBoard(std::vector <std::vector <Piece>> board);
 
 int main(int argc, char *argv[])
 {
-	// This is the enum which will fill the board array
-	// maybe in the future more options will be added 
-	// for other purposes (highlighting for analysis,
-	// etc.)
-	typedef enum 
-	{
-		EMPTY_PIECE,	// Default state with no piece
-		RED_PIECE,	// Red plays first
-		BLUE_PIECE,	// Blue plays second
-	} Piece;
 
-	char ch;
 	return 0;
+}
+
+std::string DrawBoard(std::vector <std::vector <Piece>> board) 
+{
+	return std::string {""};
 }
