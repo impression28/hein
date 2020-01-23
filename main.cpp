27bc47d007a7ee837207b2dcf286109e6aaa9ed8
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2020-01-23 19:11:13 -0300

    Draw pieces on the board

--- main.cpp
+++ main.cpp
@@ -21,11 +21,19 @@ int main(int argc, char *argv[])
 {
 	std::vector <Piece> v(11, EMPTY_PIECE);
 	std::vector <std::vector <Piece>> board(11, v);
+	board[2][3] = RED_PIECE;
+	board[5][3] = RED_PIECE;
+	board[7][6] = RED_PIECE;
+	board[5][5] = BLUE_PIECE;
+	board[1][10] = BLUE_PIECE;
+	board[8][9] = BLUE_PIECE;
 	std::cout << DrawBoard(board);
 	return 0;
 }
 
-// Preconditions: board must be 11x11
+// Preconditions: 
+//	* board must be 11x11
+//	* all lines must have exactly 11 '.'
 
 std::string DrawBoard(std::vector <std::vector <Piece>> board) 
 {
@@ -40,17 +48,48 @@ std::string DrawBoard(std::vector <std::vector <Piece>> board)
 	std::string board_template = std::string {\
 	"   A B C D E F G H I J K\n"
 	"  • • • • • • • • • • • • •\n"
-	" 1 • · · · · · · · · · · · •\n"
-	"  2 • · · · · · · · · · · · •\n"
-	"   3 • · · · · · · · · · · · •\n"
-	"    4 • · · · · · · · · · · · •\n"
-	"     5 • · · · · · · · · · · · •\n"
-	"      6 • · · · · · · · · · · · •\n"
-	"       7 • · · · · · · · · · · · •\n"
-	"        8 • · · · · · · · · · · · •\n"
-	"         9 • · · · · · · · · · · · •\n"
-	"         10 • · · · · · · · · · · · •\n"
-	"          11 • · · · · · · · · · · · •\n"
+	" 1 • . . . . . . . . . . . •\n"
+	"  2 • . . . . . . . . . . . •\n"
+	"   3 • . . . . . . . . . . . •\n"
+	"    4 • . . . . . . . . . . . •\n"
+	"     5 • . . . . . . . . . . . •\n"
+	"      6 • . . . . . . . . . . . •\n"
+	"       7 • . . . . . . . . . . . •\n"
+	"        8 • . . . . . . . . . . . •\n"
+	"         9 • . . . . . . . . . . . •\n"
+	"         10 • . . . . . . . . . . . •\n"
+	"          11 • . . . . . . . . . . . •\n"
 	"              • • • • • • • • • • • • •\n"};
+	size_t row = 0;
+	size_t column = 0;
+	for (char &c: board_template)
+	{
+		if (c == '.')
+		{
+			switch (board[row][column])
+			{
+				case RED_PIECE:
+					c = 'R';
+					break;
+				case BLUE_PIECE:
+					c = 'B';
+					break;
+				case EMPTY_PIECE:
+					break;
+			}
+			// #TODO: check if the table was well-formated
+			// for debug
+			if (column < 10)
+			{
+				column++;
+			}
+			else
+			{
+				column = 0;
+				row++;
+			}
+				
+		}
+	}
 	return std::string {board_template};
 }
